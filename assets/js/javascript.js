fb8424601a9a9e3c4c572018e3ed83f095e87d10
IsaiasQB <isaias.qb@hotmail.com>
2022-03-18 14:47:56 -0400

    trying out different event listeners ans object configuration for the answers

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,21 +1,52 @@
 // create arrays containing the questions and answers
-var questions = [
-    {Question1: "What is the meaning of life?",
-    Answer1: "Be Happy",    
-    Answer2: "Be Rich",
-    Answer3: "Be Famous",
-    Answer4: "Be Alone",
-    Correct: 1
-    },
+var questions = {
+    questions: ["What is the scientific name of a wolf?", 
+                "Which mammal has no vocal cords?",
+                "What is a female donkey called?",
+                "What is the fastest land animal in the world?",
+            ]};
 
-    {Question1: "What is the meaning of happy?",
-    Answer1: "Loving others",    
-    Answer2: "Loving your posessions",
-    Answer3: "loving yourself",
-    Answer4: "Loving your job",
-    Correct: 3
+var answers = {
+    first: [
+        "Canis lupus",
+        "Rocky Wolf",
+        "Wolfus Amadeus",
+        "Wolfverine", 
+        firstAnswers:        
+    ],
+    second: [
+        "Howling Monkey",
+        "Songbird",
+        "Giraffe",
+        "Dolfin",
+    ],
+    third: [
+        "A shedonkey",
+        "A Lady Donkey",
+        "Donkey Bonnie",
+        "A Jenny",
+    ],
+    fourth: [
+        "The Turtle",
+        "The Elephant",
+        "The Snake",
+        "The cheetah",
+    ],
+    firtsAnswers: function answers5 (){
+        var answerEl = document.createElement("li");
+        answerEl.className = "answer"
+        answerEl.textContent = answers.q.q;
+        // answerListEl.appendChild(answerEl);
+        answerListEl.innerHTML.replace(answerEl)
+
     }
-];
+};
+
+// Answer1: Canis lupus
+// Answer2: Giraffe
+// Answer3: A Jenny
+// Answer4: The cheetah
+// Answer5: 22 months"
 
 // declare global variables
 var seeScoresEl = document.querySelector(".see-scores");
@@ -24,19 +55,66 @@ var startBtnEl = document.querySelector(".start-btn");
 var answerListEl = document.querySelector(".answer-list");
 var quizHeadEl = document.querySelector(".quiz-head");
 var rightWrongMsgEl = document.querySelector(".wrong-right-msg")
-// cdeclare function for starting the quiz
-//console.log(seeScoresEl, timerDisplayEl, startBtnEl, answerListEl, quizHeadEl, rightWrongMsgEl);
-
-var startQuiz = function() {startBtnEl.addEventListener("click", function (){
-    startBtnEl.remove();
-
-    // for loop to display all the questions
-    for (var i = 0, i < questions.length; i++){
-
-    } //end of for loop
 
 
+answerListEl.addEventListener("click", loopThroughQuestions);
+
+
+
+
+//loop through questions
+var loopThroughQuestions = function(event){
+
+    var answereClicked = event.target
+
+    if (answereClicked.matches(".answer")) {
+        console.log("an answer was cliked")
+    }
+
+} //end of loopThroughQuestions
+
+
+
+var q = 0
+// event listener for the estar button
+startBtnEl.addEventListener("click", function (){
+
+    
+
+            startBtnEl.textContent = "Next Question"
+
+            //set the first question
+            var questionTitleEl = document.createElement("h1");
+            questionTitleEl.className = "quiz-title"
+            questionTitleEl.textContent = questions.questions[q];
+            quizHeadEl.appendChild(questionTitleEl);
+
+            
+            q++
+
+
+
+            for (var i = 0; i < questions.questions.length; i++){
+            //create the answers for each question and append it to the quiz head
+            var answerEl = document.createElement("li");
+            answerEl.className = "answer"
+            answerEl.textContent = answers.q.q;
+            // answerListEl.appendChild(answerEl);
+            answerListEl.innerHTML.replace(answerEl)
+        }
+    
 });
-}  // end of startQuiz fn
 
-startQuiz()
+
+//event listener when an aswer is clicked
+answerListEl.addEventListener("click", function (event){
+    var answerClicked = event.target
+    var answerClass = answerClicked.getAttribute("class") 
+
+    if(answerClass === "answer"){
+        console.log(answerClass);
+    }else {
+        console.log("no answer clicked");
+    }
+}
+);
